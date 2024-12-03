4b21dda466025b0f5e74a8f16b50b571ec5c8290
Amiel Esplana <Amiel2.Esplana@live.uwe.ac.uk>
2024-12-03 15:45:07 +0000

    checkpoint 202412031545

---
+++ notebooks/ds_mapping/aml.py
@@ -0,0 +1,22 @@
+txn_mapping = {
+    "CASH-DEPOSIT": {
+        "std_txn_type": "DEPOSIT",
+        "std_txn_method": "CASH"
+    },
+    "WITHDRAWL": {
+        "std_txn_type": "WITHDRAWAL",
+        "std_txn_method": "CASH"
+    },
+    "WIRE": {
+        "std_txn_type": "TRANSFER",
+        "std_txn_method": "ELECTRONIC"
+    },
+    "PAYMENT": {
+        "std_txn_type": "PAYMENT",
+        "std_txn_method": "ELECTRONIC"
+    },
+    "CRYPTO-TRANSFER": {
+        "std_txn_type": "TRANSFER",
+        "std_txn_method": "ELECTRONIC"
+    }
+}
---
+++ notebooks/ds_mapping/fraud.py
@@ -0,0 +1,66 @@
+txn_mapping = {
+  "QUICK-PAYMENT-QUICK-PAYMENT": {
+    "std_txn_type": "PAYMENT",
+    "std_txn_method": "ELECTRONIC"
+  },
+  "DEPOSIT-CASH-DEPOSIT-CASH": {
+    "std_txn_type": "DEPOSIT",
+    "std_txn_method": "CASH"
+  },
+  "PAY-CHECK-PAY-CHECK": {
+    "std_txn_type": "PAYMENT",
+    "std_txn_method": "CHECK"
+  },
+  "DEPOSIT-CHECK-DEPOSIT-CHECK": {
+    "std_txn_type": "DEPOSIT",
+    "std_txn_method": "CHECK"
+  },
+  "MAKE-PAYMENT-MAKE-PAYMENT": {
+    "std_txn_type": "PAYMENT",
+    "std_txn_method": "ELECTRONIC"
+  },
+  "MOVE-FUNDS-MOVE-FUNDS": {
+    "std_txn_type": "TRANSFER",
+    "std_txn_method": "ELECTRONIC"
+  },
+  "DEPOSIT-CASH-QUICK-DEPOSIT": {
+    "std_txn_type": "DEPOSIT",
+    "std_txn_method": "CASH"
+  },
+  "DEPOSIT-CHECK-CASH-CHECK": {
+    "std_txn_type": "DEPOSIT",
+    "std_txn_method": "CHECK"
+  },
+  "MAKE-PAYMENT-PAY-BILL": {
+    "std_txn_type": "PAYMENT",
+    "std_txn_method": "ELECTRONIC"
+  },
+  "WITHDRAWAL-WITHDRAWAL": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CASH"
+  },
+  "MAKE-PAYMENT-PAYMENT": {
+    "std_txn_type": "PAYMENT",
+    "std_txn_method": "ELECTRONIC"
+  },
+  "WITHDRAWAL-CASH-CHECK": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CHECK"
+  },
+  "EXCHANGE-CASH-CHECK": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CHECK"
+  },
+  "EXCHANGE-WITHDRAWAL": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CHECK"
+  },
+  "EXCHANGE-EXCHANGE": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CHECK"
+  },
+  "WITHDRAWAL-EXCHANGE": {
+    "std_txn_type": "WITHDRAWAL",
+    "std_txn_method": "CHECK"
+  }
+}
